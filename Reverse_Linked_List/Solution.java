e930e074556c9e6e9b36e917f3a7c27463bf6730
Eduardo Aguiar <eduardocamposaguiar57@gmail.com>
2026-07-20 12:09:04 -0300

    Add Reverse Linked List solution

---
+++ Reverse_Linked_List/Solution.java
@@ -0,0 +1,17 @@
+package Reverse_Linked_List;
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        
+        while (curr != null) {
+            ListNode nextTemp = curr.next; 
+            curr.next = prev;              
+            prev = curr;                   
+            curr = nextTemp;               
+        }
+        
+        return prev; 
+    }
+}
